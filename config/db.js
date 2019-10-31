1b7d5255adfcf6c52626f7e1c388a3a98ac55d17
ayodeleoniosun <heywire4u@gmail.com>
2019-10-31 03:18:57 +0100

    Changed database connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/contacts');
+//mongoose.connect('mongodb://localhost:27017/contacts');
+mongoose.connect('mongodb+srv://ayodele:553534123@cluster0-yhqco.mongodb.net/contacts');
 
 mongoose.connection.on('connected', () => {
     console.log('connected to mongo database');
